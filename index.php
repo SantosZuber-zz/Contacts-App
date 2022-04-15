c981fa1599beaab842210f859e690964545dbbf8
Santos Zuberbuhler <santoszuberbuhler2003@gmail.com>
2022-04-15 10:53:01 -0300

    store contacts

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
 
-$contacts = [
-  ["name" => "Pepe", "phone_number" => "2132139"],
-  ["name" => "Antonio", "phone_number" => "982392"],
-  ["name" => "Nate", "phone_number" => "329847"],
-  ["name" => "Rodrigo", "phone_number" => "4353234"],
-  ["name" => "Marcos", "phone_number" => "12312432"],
-];
+require "database.php";
+
+$contacts = $conn->query("SELECT * FROM contacts");
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,10 +13,14 @@ $contacts = [
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./css/style.css">
+
   <!-- Bootstrap -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.1.3/darkly/bootstrap.min.css" integrity="sha512-ZdxIsDOtKj2Xmr/av3D/uo1g15yxNFjkhrcfLooZV5fW0TT7aF7Z3wY1LOA16h0VgFLwteg14lWqlYUQK3to/w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" defer></script>
+  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  <!-- Static Content -->
+  <link rel="stylesheet" href="./css/style.css" />
+
   <title>Contacts App</title>
 </head>
 
@@ -27,7 +28,7 @@ $contacts = [
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
       <a class="navbar-brand font-weight-bold" href="#">
-        <img class="mr-2" src="./assets/img/logo.png" />
+        <img class="mr-2" src="./assets//img/logo.png" />
         ContactsApp
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -36,22 +37,28 @@ $contacts = [
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="./index.html">Home</a>
+            <a class="nav-link" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="./views/add.html">Add Contact</a>
+            <a class="nav-link" href="add.php">Add Contact</a>
           </li>
         </ul>
       </div>
     </div>
   </nav>
 
-
-
   <main>
     <div class="container pt-4 p-3">
       <div class="row">
 
+        <?php if ($contacts->rowCount() == 0) : ?>
+          <div class="col-md-4 mx-auto">
+            <div class="card card-body text-center">
+              <p>No contacts saved yet</p>
+              <a href="add.php">Add One!</a>
+            </div>
+          </div>
+        <?php endif ?>
         <?php foreach ($contacts as $contact) : ?>
           <div class="col-md-4 mb-3">
             <div class="card text-center">
